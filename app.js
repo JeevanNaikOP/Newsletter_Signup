8ab201a0bcd72622d7d21f6b28a8d8fb26f414f0
JeevanNaikOP <61685578+JeevanNaikOP@users.noreply.github.com>
2020-09-03 19:02:15 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,55 @@
+const express=require('express');
+const bodyParser=require("body-parser");
+const request=require('request');
+const https=require("https")
+
+const app=express();
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+app.get("/",function(req,res){
+  res.sendFile(__dirname+"/signup.html");
+});
+app.post("/",function(req,res){
+  const firstName=req.body.fname;
+  const lastName=req.body.lname;
+  const email=req.body.email;
+  const data={
+    members:[
+      {
+        email_address:email,
+        status:"subscribed",
+        merge_fields:{
+          FNAME:firstName,
+          LNAME:lastName
+        }
+      }
+    ]
+  };
+  const jsonData=JSON.stringify(data);
+  const url="https://us17.api.mailchimp.com/3.0/lists/fd1cb4f7d9";
+  const options={
+    method:"POST",
+    auth:"jeevan1:f24ebd83f09761eeebd43dc545a03e1b-us17"
+  }
+  const request = https.request(url,options,function(response){
+    if(response.statusCode===200){
+      res.sendFile(__dirname+"/success.html")
+    }
+    else{
+      res.sendFile(__dirname+"/failure.html")
+    }
+response.on("data",function(data){
+  console.log(JSON.parse(data));
+})
+})
+request.write(jsonData);
+request.end();
+});
+app.post("/failure",function(req,res){
+  res.redirect("/")
+})
+app.listen(process.env.PORT || 3000 ,function(){
+  console.log("server is running");
+})
+// f24ebd83f09761eeebd43dc545a03e1b-us17
+//fd1cb4f7d9
